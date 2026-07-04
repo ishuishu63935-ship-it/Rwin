b332aa095ce466cb4d3aeb433a4331b262a6e00b
ishuishu63935-ship-it <ishuishu63935@gmail.com>
2026-07-05 02:24:32 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,36 +5,16 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyAedMCjwyX8yB-zK4hc0RgQ0LaziUj_4io",
-    authDomain: "rwin-e6021.firebaseapp.com",
-    projectId: "rwin-e6021",
-    storageBucket: "rwin-e6021.firebasestorage.app",
-    messagingSenderId: "677090984731",
-    appId: "1:677090984731:web:9a61a1ec166e193617c9be",
-    measurementId: "G-RCR9MT2KV6"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+// Firebase Config
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAedMCjwyX8yB-zK4hc0RgQ0LaziUj_4io",
+  authDomain: "rwin-e6021.firebaseapp.com",
+  projectId: "rwin-e6021",
+  storageBucket: "rwin-e6021.firebasestorage.app",
+  messagingSenderId: "677090984731",
+  appId: "1:677090984731:web:9a61a1ec166e193617c9be",
+  measurementId: "G-RCR9MT2KV6"
 };
 
 // Firebase Start
@@ -51,11 +31,7 @@ const status = document.getElementById("loginStatus");
 // Signup
 if (signupBtn) {
   signupBtn.addEventListener("click", () => {
-    createUserWithEmailAndPassword(
-      auth,
-      email.value,
-      password.value
-    )
+    createUserWithEmailAndPassword(auth, email.value, password.value)
       .then(() => {
         status.innerHTML = "✅ Account Created";
         alert("Account Created Successfully");
@@ -69,11 +45,7 @@ if (signupBtn) {
 // Login
 if (loginBtn) {
   loginBtn.addEventListener("click", () => {
-    signInWithEmailAndPassword(
-      auth,
-      email.value,
-      password.value
-    )
+    signInWithEmailAndPassword(auth, email.value, password.value)
       .then(() => {
         status.innerHTML = "✅ Login Success";
         alert("Login Successful");
