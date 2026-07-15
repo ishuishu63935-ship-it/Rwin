be6519aed68688119e3cb5f9803d65776fba8f4e
ishuishu63935-ship-it <ishuishu63935@gmail.com>
2026-07-16 00:54:45 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -26,3 +26,42 @@ import {
 const firebaseConfig = {
 
  
+  apiKey: "YAHA_APNI_API_KEY",
+
+  authDomain: "rwin-e6021.firebaseapp.com",
+
+  projectId: "rwin-e6021",
+
+  storageBucket: "rwin-e6021.appspot.com",
+
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+
+  appId: "YOUR_APP_ID"
+
+};
+
+// ================================
+// PART 3 - INITIALIZE
+// ================================
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const db = getFirestore(app);
+
+// ================================
+// PART 4 - ELEMENTS
+// ================================
+
+const email = document.getElementById("email");
+
+const password = document.getElementById("password");
+
+const loginBtn = document.getElementById("loginBtn");
+
+const signupBtn = document.getElementById("signupBtn");
+
+const status = document.getElementById("loginStatus");
+
+console.log("✅ Firebase Ready");
