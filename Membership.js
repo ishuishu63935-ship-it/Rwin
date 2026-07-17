c7946a9516765eba16491d75febb7d7c53b16c7d
ishuishu63935-ship-it <ishuishu63935@gmail.com>
2026-07-18 04:54:03 +0530

    Update Membership.js

--- Membership.js
+++ Membership.js
@@ -1,1 +1,47 @@
+"use strict";
 
+const starterBtn = document.getElementById("buy21");
+const proBtn = document.getElementById("buy60");
+const eliteBtn = document.getElementById("buy365");
+
+function openPayment(plan, price) {
+
+    localStorage.setItem("selectedPlan", plan);
+
+    localStorage.setItem("selectedPrice", price);
+
+    window.location.href = "payment.html";
+
+}
+
+if (starterBtn) {
+
+    starterBtn.onclick = () => {
+
+        openPayment("Starter", 49);
+
+    };
+
+}
+
+if (proBtn) {
+
+    proBtn.onclick = () => {
+
+        openPayment("Pro", 399);
+
+    };
+
+}
+
+if (eliteBtn) {
+
+    eliteBtn.onclick = () => {
+
+        openPayment("Elite", 3999);
+
+    };
+
+}
+
+console.log("✅ Membership Ready");
